0355b317ab5b4e40342a9593314ef583a466d1e1
centrodph <centrodph@gmail.com>
2023-03-07 11:58:53 -0300

    chore: test publish

--- pages/api/publish.js
+++ pages/api/publish.js
@@ -6,11 +6,11 @@ export default async function handler(req, res) {
     // e.g. for "/blog/[slug]" this should be "/blog/post-1"
     await res.revalidate("/index");
     await res.revalidate("page/[page]");
-    await res.revalidate("[...slug]");
+    await res.revalidate("[...url]");
     return res.json({ revalidated: true });
   } catch (err) {
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
-    return res.status(500).send("Error revalidating");
+    return res.status(500).send("Error revalidating" + err);
   }
 }
